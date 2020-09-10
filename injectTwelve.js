9c881a0df1d0d1c47a1e879988d1d2cf910ddadb
Adhil Juvane <adhilmp3@gmail.com>
2020-09-10 23:45:35 +0530

    Create injectTwelve.js

---
+++ injectTwelve.js
@@ -0,0 +1,32 @@
+function callData(){
+    var phone = "9048559097";
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        console.log('The request success!', xhr.response);
+		    phone = xhr.response;
+        phone = phone.replace(/['"]+/g, '');
+        console.log(phone);
+        a.href=`https://wa.me/91${phone}?text=${currentPage}%20I%20would%20like%20to%20know%20more%20about%20this`;
+		currentPage = location.href;
+	   } else {
+		    console.log('The request failed!');
+       phone = "Error";
+       a.href=`https://wa.me/91${phone}?text=${currentPage}%20I%20would%20like%20to%20know%20more%20about%20this`
+     }
+  };
+  xhr.open('GET', 'https://13916c2d620b.ngrok.io/getWatsappNumber/zoko-dev.myshopify.com');
+  xhr.send();
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://codepen.io/aj-zoko/pen/XWdERRZ.css";
+    document.head.prepend(link);
+    let a = document.createElement("a");
+    a.id = "watsapp-button";
+    a.href=`https://wa.me/91${phone}?text=Hellloooo`;
+		currentPage = location.href;
+	a.href=`https://wa.me/91${phone}?text=${currentPage}%20I%20would%20like%20to%20know%20more%20about%20this`
+    document.body.append(a);
+}
+
+callData();
