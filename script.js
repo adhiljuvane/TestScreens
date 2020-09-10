62eb503007c05c8b288b7b0c9884ce489d2a8ae4
Adhil Juvane <adhilmp3@gmail.com>
2020-09-10 13:41:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = "https://codepen.io/aj-zoko/pen/XWdERRZ.css";
 let a = document.createElement("a");
+console.log("weeeeeeee")
 a.id = "watsapp-button";
 document.head.prepend(link);
 document.body.append(a);
