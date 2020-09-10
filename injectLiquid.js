e99ea2cf1dd539f4cb532445d5d365bb80dc5d44
Adhil Juvane <adhilmp3@gmail.com>
2020-09-10 14:51:46 +0530

    Create injectLiquid.js

---
+++ injectLiquid.js
@@ -0,0 +1,26 @@
+function callData(){
+     let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://codepen.io/aj-zoko/pen/XWdERRZ.css";
+    document.head.prepend(link);
+    let a = document.createElement("a");
+    a.id = "watsapp-button";
+    document.body.append(a);
+  if(request.page_type == 'product'){
+    document.getElementById('watsapp-button').href="href='https://wa.me/919048559096?text=Info%20Variant%20{{product.selected_or_first_available_variant.id}}"
+  }else{
+    document.getElementById('watsapp-button').href="https://wa.me/919048559096?text=General"
+  }
+  setInterval(function()
+	{
+    	if (currentPage != location.href)
+    	{
+        	currentPage = location.href;
+            var pos = currentPage.indexOf('variant')+8;
+          	var variant_id = currentPage.slice(pos)
+          	document.getElementById('watsapp-button').href = 'https://wa.me/919048559096?text=Info%20Variant%20'+variant_id;
+    	}
+	}, 500);
+}
+
+callData();
