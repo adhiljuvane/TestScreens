fadec89d343d1c2c0c78c0a611ebb2df986ec9e5
Adhil Juvane <adhilmp3@gmail.com>
2020-09-10 15:57:41 +0530

    Create injectFourth.js

---
+++ injectFourth.js
@@ -0,0 +1,33 @@
+function callData(){
+     let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = "https://codepen.io/aj-zoko/pen/XWdERRZ.css";
+    document.head.prepend(link);
+    let a = document.createElement("a");
+    a.id = "watsapp-button";
+    a.href="https://wa.me/919048559096?text=Hellloooo"
+	  {%- if request.page_type == 'product' -%}
+    a.href="https://wa.me/919048559096?text=Info%20Variant%20{{product.selected_or_first_available_variant.id}}"
+  {%- else -%}
+    a.href="https://wa.me/919048559096?text=General"
+  {%- endif -%}
+  if({{request.page_type}} == 'product'){
+    document.getElementById('watsapp-button').href="href='https://wa.me/919048559096?text=Info%20Variwnt%20{{product.selected_or_first_available_variant.id}}"
+  }else{
+    document.getElementById('watsapp-button').href="https://wa.me/919048559096?text=Gener1l"
+  }
+  setInterval(function()
+	{
+    	if (currentPage != location.href)
+    	{
+        	currentPage = location.href;
+            var pos = currentPage.indexOf('variant')+8;
+          	var variant_id = currentPage.slice(pos)
+          	a.href = 'https://wa.me/919048559096?text=Info%20Variant%20'+variant_id;
+    	}
+	}, 500);
+    document.body.append(a);
+
+}
+
+callData();
